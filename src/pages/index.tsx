e620262694be8436e32e7dab5be01b491e71d68d
Abhishek Angira <abhishek@angira.net>
2021-06-18 15:38:50 +0530

    fixed title padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const tagline = "Padosi helps you connect to your neighbors and community.";
 export default function Home() {
   return (
     <>
-      <div className="h-full pt-32 lg:pt-64 mx-auto max-w-6xl px-5 flex flex-col lg:flex-row gap-y-9 lg:gap-x-28 lg:justify-center items-center lg:items-start">
+      <div className="h-full pt-[15vh] lg:pt-64 mx-auto max-w-6xl px-5 flex flex-col lg:flex-row gap-y-9 lg:gap-x-28 lg:justify-center items-center lg:items-start">
         <div className="grid gap-y-4 text-center max-w-lg lg:text-left">
           <h1 className="font-acme text-5xl text-primary">Padosi</h1>
           <h2 className="hidden text-2xl font-bold lg:block dark:text-primary-light">{tagline}</h2>
