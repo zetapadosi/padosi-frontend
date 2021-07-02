d8b2e70b7f86138df32886bf347ab50a5f90a15e
Abhishek Angira <abhishek@angira.net>
2021-07-02 20:44:42 +0530

    placed prefetching logic in home

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,7 +1,17 @@
 import PostCard from "../PostCard";
 import MobileHome from "../layouts/MobileHome";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function MainPage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.prefetch("/profile");
+    router.prefetch("/search");
+    router.prefetch("/settings");
+    router.prefetch("/new-post");
+  }, []);
+
   return (
     <MobileHome home>
       <div className="flex flex-col gap-4">
