a7f71c3cc35a2caeec910cce8275038efec9c530
Abhishek Angira <abhishek@angira.net>
2021-07-16 12:20:43 +0530

    Updated preview

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,37 +1,75 @@
-import Cookies from "js-cookie";
+import axios from "../utils/axiosInstance";
 
-export const getWallPosts = async (userId) => {
+export const getWallPosts = async (page: number) => {
   try {
-    let response = await fetch(
-      "https://padosi-backend.herokuapp.com/api/post/wall/" + userId + "?page=0&limit=10",
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + Cookies.get("t"),
-        },
-      }
+    let response = await axios.get(
+      `https://padosi-backend.herokuapp.com/api/post/wall?page=${page}&limit=10`
     );
-    return await response.json();
+    return response.data.value;
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
-export const createPost = async (userId, post) => {
+export const getPost = async (postId: string) => {
   try {
-    let response = await fetch("https://padosi-backend.herokuapp.com/api/post/create/" + userId, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: "Bearer " + Cookies.get("t"),
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(post),
-    });
-    return await response.json();
+    let response = await axios.get(`post/view/${postId}`);
+    return response.data.value;
   } catch (err) {
-    console.log(err);
+    console.error(err);
+  }
+};
+
+export const createPost = async (post: { tags: string[]; postText: string }) => {
+  try {
+    let response = await axios.post("post/create", post);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const searchPostsByTags = async (page: number, tags: { tags: string[] }) => {
+  try {
+    let response = await axios.post(`post/search?page=${page}&limit=10`, tags);
+    return response.data.value;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const likePost = async (postIdObj: { postId: string }) => {
+  try {
+    let response = await axios.put("post/like", postIdObj);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const unlikePost = async (postIdObj: { postId: string }) => {
+  try {
+    let response = await axios.put("post/unlike", postIdObj);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const addComment = async (commentData: { commentText: string; postId: string }) => {
+  try {
+    let response = await axios.put("post/comment", commentData);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const deleteComment = async (commentData: { commentId: string; postId: string }) => {
+  try {
+    let response = await axios.put("post/uncomment", commentData);
+    return response.data;
+  } catch (err) {
+    console.error(err);
   }
 };
